1c4a2f6857611225a75e26b01afc8e4182175ef2
BorgesAbraao10 <abraao10clear@gmail.com>
2024-05-13 17:07:40 -0300

    Last updade

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import Nome from './components/Nome'
 
-
-function App() {
-  return (
+function App(){
+  return(
     <div>
-     <h1>Welcome my first project and work</h1>
+      <h1>Componente App</h1>
+      <Nome aluno="Lucas" idade= {30} />
+      <br />
+      <Nome aluno="Joao Victor" idade= {25}/>
     </div>
+    
   );
 }
 
 export default App;
+
+
