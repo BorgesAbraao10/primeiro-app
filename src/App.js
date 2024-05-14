b972e8f059bd366715e8695bec713b461299b9e9
BorgesAbraao10 <abraao10clear@gmail.com>
2024-05-14 08:53:28 -0300

    Last update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
+
 import Nome from './components/Nome'
 
 function App(){
+  const [aluno, setAluno] = useState('Sujeito Programador')
   return(
     <div>
-      <h1>Componente App</h1>
+      <h1>Componente App</h1> <br/>
+      <h2>Olá: {aluno} </h2>
       <Nome aluno="Lucas" idade= {30} />
-      <br />
-      <Nome aluno="Joao Victor" idade= {25}/>
+    
     </div>
     
   );
