2055fb37622eed4eed02ec7e1aabc3d9c9b49d20
BorgesAbraao10 <abraao10clear@gmail.com>
2024-05-14 09:22:11 -0300

    Last update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { useState } from 'react';
 
-import Nome from './components/Nome'
 
 function App(){
   const [aluno, setAluno] = useState('Sujeito Programador')
+
+  function mudarNome(nome) {
+    setAluno(nome)
+  }
+
   return(
     <div>
       <h1>Componente App</h1> <br/>
       <h2>Olá: {aluno} </h2>
-      <Nome aluno="Lucas" idade= {30} />
+      <button onClick={ ()=> mudarNome ('Abraão Borges') }>
+        Mudar Nome
+      </button>
+      <br />
+      <br />
+      
     
     </div>
     
